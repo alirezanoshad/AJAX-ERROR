25ce9ab7e7b6141612dd796ac19b6c0af64c85c3
Alireza <iranexplosm@gmail.com>
2024-12-31 16:09:39 +0330

    html and js comments fixed

--- main.js
+++ main.js
@@ -1,34 +1,30 @@
 "use strict";
-// HTML Refrences
+// HTML Refrences & Their Event Listener
 const E200 = document.querySelector(".E200");
 E200.addEventListener("click", meow200);
-
 const E201 = document.querySelector(".E201");
 E201.addEventListener("click", meow201);
-
 const E300 = document.querySelector(".E300");
 E300.addEventListener("click", meow300);
-
 const E404 = document.querySelector(".E404");
 E404.addEventListener("click", meow404);
-
 const E500 = document.querySelector(".E500");
 E500.addEventListener("click", meow500);
-
+// Mp3 Refrence
 const mahboob = document.querySelector(".mahboob");
-
+// List refrence
 const ErrorResponse = document.querySelector(".ErrorResponse");
-
+//200 ajax
 function meow200() {
   const xhr = new XMLHttpRequest();
-
   xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/", true);
-
+  //200 ajax Event Listener
   xhr.onreadystatechange = function meow200huh() {
     if (this.readyState === XMLHttpRequest.DONE) {
       if (this.status === 200) {
         console.log("done", this.status, this.responseText);
         ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status}<br>"200" stands for "OK" The request was successful and server  sent back the data correctly</span>`;
+        // SweetAlert
         Swal.fire({
           title: "OK!",
           icon: "success",
@@ -42,20 +38,20 @@ function meow200() {
   };
   xhr.send();
 }
-
+// 201 ajax
 function meow201() {
   const xhr = new XMLHttpRequest();
-
   xhr.open("POST", "https://jsonplaceholder.typicode.com/posts/", true);
-
+  // 201 ajax Event Listener
   xhr.onreadystatechange = function meow201huh() {
     console.log(this);
     console.log(XMLHttpRequest.DONE);
-
+    //201 ajax Event Listener
     if (this.readyState === XMLHttpRequest.DONE) {
       if (this.status === 201) {
         console.log("done", this.status, this.responseText);
         ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status}<br>the HTTP request has led to the creation of a resource</span>`;
+        // SweetAlert
         Swal.fire({
           title: "Created!",
           icon: "success",
@@ -69,20 +65,19 @@ function meow201() {
   };
   xhr.send();
 }
-
+// 300 ajax
 function meow300() {
   const xhr = new XMLHttpRequest();
-
   xhr.open("GET", "https://httpstat.us/300", true);
-
+  // 300 ajax Event Listener
   xhr.onreadystatechange = function meow300huh() {
     console.log(this);
     console.log(XMLHttpRequest.DONE);
-
     if (this.readyState === XMLHttpRequest.DONE) {
       if (this.status === 300) {
         console.log("done", this.status, this.responseText);
         ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status} <br> ${this.responseText}</span>`;
+        // SweetAlert
         Swal.fire({
           title: "The request has more than one possible response",
           text: "Error_300",
@@ -96,29 +91,29 @@ function meow300() {
   };
   xhr.send();
 }
-
+// 404 ajax
 function meow404() {
   console.log("popopopopopopo");
   const xhr = new XMLHttpRequest();
 
   xhr.open("GET", "https://httpstat.us/404", true);
-
+  // 404 ajax Event Listener
   xhr.onreadystatechange = function meow404huh() {
     console.log(this);
     console.log(XMLHttpRequest.DONE);
-
     if (this.readyState === XMLHttpRequest.DONE) {
       if (this.status === 404) {
         console.log("done", this.status, this.responseText);
         ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status} <br> ${this.responseText}</span>`;
+        // SweetAlert
         Swal.fire({
-            title: "Oops...",
-            text: "Error_404(not found)",
-            imageUrl: "docs/myFriend2.jpg",
-            imageWidth: 400,
-            imageHeight: 480,
-            imageAlt: "Custom image",
-          });
+          title: "Oops...",
+          text: "Error_404(not found)",
+          imageUrl: "docs/myFriend2.jpg",
+          imageWidth: 400,
+          imageHeight: 480,
+          imageAlt: "Custom image",
+        });
         mahboob.play();
       } else {
         console.log("notdone", this.status, this.responseText);
@@ -128,21 +123,20 @@ function meow404() {
   };
   xhr.send();
 }
-
+// 500 ajax
 function meow500() {
   console.log("popopopopopopo");
   const xhr = new XMLHttpRequest();
-
   xhr.open("GET", "https://httpstat.us/500", true);
-
+  // 500 ajax
   xhr.onreadystatechange = function meow500huh() {
     console.log(this);
     console.log(XMLHttpRequest.DONE);
-
     if (this.readyState === XMLHttpRequest.DONE) {
       if (this.status === 500) {
         console.log("done", this.status, this.responseText);
         ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status} <br> ${this.responseText}</span>`;
+        // SweetAlert
         Swal.fire({
           title: "Internal Server Error",
           text: "❤از صبر و شکیبایی شما سپاسگرازیم",
