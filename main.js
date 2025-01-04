fe2df4f1890247fd896a84a652f73d5fa5d02102
Alireza <iranexplosm@gmail.com>
2025-01-04 12:49:45 +0330

    js updated
    -names are formal now
    optimized code and shorter

--- main.js
+++ main.js
@@ -1,154 +1,71 @@
 "use strict";
 // HTML Refrences & Their Event Listener
-const E200 = document.querySelector(".E200");
-E200.addEventListener("click", meow200);
-const E201 = document.querySelector(".E201");
-E201.addEventListener("click", meow201);
-const E300 = document.querySelector(".E300");
-E300.addEventListener("click", meow300);
-const E404 = document.querySelector(".E404");
-E404.addEventListener("click", meow404);
-const E500 = document.querySelector(".E500");
-E500.addEventListener("click", meow500);
+const error200Btn = document.querySelector(".E200");
+error200Btn.addEventListener("click", function () {
+  ajaxFunction("GET", "https://jsonplaceholder.typicode.com/posts/");
+});
+const error201Btn = document.querySelector(".E201");
+error201Btn.addEventListener("click", function () {
+  ajaxFunction("POST", "https://jsonplaceholder.typicode.com/posts/");
+});
+const error300Btn = document.querySelector(".E300");
+error300Btn.addEventListener("click", function () {
+  ajaxFunction("GET", "https://httpstat.us/300");
+});
+const error404Btn = document.querySelector(".E404");
+error404Btn.addEventListener("click", function () {
+  ajaxFunction("GET", "https://httpstat.us/404");
+});
+const error500Btn = document.querySelector(".E500");
+error500Btn.addEventListener("click", function () {
+  ajaxFunction("GET", "https://httpstat.us/500");
+});
 // Mp3 Refrence
-const mahboob = document.querySelector(".mahboob");
+const mp3 = document.querySelector(".mahboob");
 // List refrence
 const ErrorResponse = document.querySelector(".ErrorResponse");
+
 //200 ajax
-function meow200() {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/", true);
+function ajaxFunction(requestType, myUrl) {
+  const xhr = new XMLHttpRequest(requestType, myUrl);
+  xhr.open(requestType, myUrl, true);
   //200 ajax Event Listener
-  xhr.onreadystatechange = function meow200huh() {
+  xhr.onreadystatechange = function ajaxValidation() {
     if (this.readyState === XMLHttpRequest.DONE) {
-      if (this.status === 200) {
-        console.log("done", this.status, this.responseText);
-        ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status}<br>"200" stands for "OK" The request was successful and server  sent back the data correctly</span>`;
-        // SweetAlert
-        Swal.fire({
-          title: "OK!",
-          icon: "success",
-          draggable: true,
-        });
-      } else {
-        console.log("notdone", this.status, this.responseText);
-        ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status}</span>`;
-      }
-    }
-  };
-  xhr.send();
-}
-// 201 ajax
-function meow201() {
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://jsonplaceholder.typicode.com/posts/", true);
-  // 201 ajax Event Listener
-  xhr.onreadystatechange = function meow201huh() {
-    console.log(this);
-    console.log(XMLHttpRequest.DONE);
-    //201 ajax Event Listener
-    if (this.readyState === XMLHttpRequest.DONE) {
-      if (this.status === 201) {
-        console.log("done", this.status, this.responseText);
-        ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status}<br>the HTTP request has led to the creation of a resource</span>`;
-        // SweetAlert
-        Swal.fire({
-          title: "Created!",
-          icon: "success",
-          draggable: true,
-        });
-      } else {
-        console.log("notdone", this.status, this.responseText);
-        ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status} ${this.responseText}</span>`;
-      }
-    }
-  };
-  xhr.send();
-}
-// 300 ajax
-function meow300() {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://httpstat.us/300", true);
-  // 300 ajax Event Listener
-  xhr.onreadystatechange = function meow300huh() {
-    console.log(this);
-    console.log(XMLHttpRequest.DONE);
-    if (this.readyState === XMLHttpRequest.DONE) {
-      if (this.status === 300) {
-        console.log("done", this.status, this.responseText);
-        ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status} <br> ${this.responseText}</span>`;
-        // SweetAlert
-        Swal.fire({
+      console.log(this.status, this.responseText);
+      ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status}</span>`;
+      // SweetAlert Validation
+      const swalFireTypes = {
+        200: { title: "OK!", icon: "success", draggable: true },
+        201: { title: "Created!", icon: "success", draggable: true },
+        300: {
           title: "The request has more than one possible response",
           text: "Error_300",
           icon: "question",
-        });
-      } else {
-        console.log("notdone", this.status, this.responseText);
-        ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status} <br> ${this.responseText}</span>`;
-      }
-    }
-  };
-  xhr.send();
-}
-// 404 ajax
-function meow404() {
-  console.log("popopopopopopo");
-  const xhr = new XMLHttpRequest();
-
-  xhr.open("GET", "https://httpstat.us/404", true);
-  // 404 ajax Event Listener
-  xhr.onreadystatechange = function meow404huh() {
-    console.log(this);
-    console.log(XMLHttpRequest.DONE);
-    if (this.readyState === XMLHttpRequest.DONE) {
-      if (this.status === 404) {
-        console.log("done", this.status, this.responseText);
-        ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status} <br> ${this.responseText}</span>`;
-        // SweetAlert
-        Swal.fire({
+        },
+        404: {
           title: "Oops...",
           text: "Error_404(not found)",
           imageUrl: "docs/myFriend2.jpg",
           imageWidth: 400,
           imageHeight: 480,
           imageAlt: "Custom image",
-        });
-        mahboob.play();
-      } else {
-        console.log("notdone", this.status, this.responseText);
-        ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status} <br> ${this.responseText}</span>`;
-      }
-    }
-  };
-  xhr.send();
-}
-// 500 ajax
-function meow500() {
-  console.log("popopopopopopo");
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://httpstat.us/500", true);
-  // 500 ajax
-  xhr.onreadystatechange = function meow500huh() {
-    console.log(this);
-    console.log(XMLHttpRequest.DONE);
-    if (this.readyState === XMLHttpRequest.DONE) {
-      if (this.status === 500) {
-        console.log("done", this.status, this.responseText);
-        ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status} <br> ${this.responseText}</span>`;
-        // SweetAlert
-        Swal.fire({
+        },
+        500: {
           title: "Internal Server Error",
           text: "❤از صبر و شکیبایی شما سپاسگرازیم",
           imageUrl: "docs/myFriend.jpg",
           imageWidth: 400,
           imageHeight: 400,
           imageAlt: "Custom image",
-        });
-        mahboob.play();
-      } else {
-        console.log("notdone", this.status, this.responseText);
-        ErrorResponse.innerHTML += `<span class="text-light text-center rounded-3" style="font-size: 30px;">error code::${this.status} <br> ${this.responseText}</span>`;
+        },
+      };
+      const response = swalFireTypes[this.status];
+      if (response) {
+        Swal.fire(response);
+        if (this.status === 404 || this.status === 500) {
+          mp3.play();
+        }
       }
     }
   };
